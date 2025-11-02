7f5f99907ccde5a9daa6d6bbe514fe4b8f3a51a3
Olivier Dehareng <olivier.d1000@gmail.com>
2025-11-02 17:18:56 +0100

    Tidy up the create with settings

--- src/instances_app.rs
+++ src/instances_app.rs
@@ -560,3 +560,13 @@ impl InstanceApp {
                     binding: 1,
                     visibility: wgpu::ShaderStages::COMPUTE,
                     ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Storage { read_only: false },
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+                // Binding 2: time uniform (currently unused)
+                wgpu::BindGroupLayoutEntry {
+                    binding: 2,
+                    visibility: wgpu::ShaderStages::COMPUTE,
